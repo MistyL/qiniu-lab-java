f864569055313577c522f6a593e5930085980f41
Jemy <jemygraw@gmail.com>
2015-01-13 11:37:46 +0800

    Add domain config.

--- src/com/qiniu/lab/config/LabConfig.java
+++ src/com/qiniu/lab/config/LabConfig.java
@@ -4,7 +4,7 @@ public class LabConfig {
 	public final static String ACCESS_KEY = "ELUs327kxVPJrGCXqWae9yioc0xYZyrIpbM6Wh6o";
 	public final static String SECRET_KEY = "LVzZY2SqOQ_I_kM1n00ygACVBArDvOWtiLkDtKi_";
 	public final static String PUBLIC_BUCKET = "if-pbl";
-	public final static String PUBLIC_BUCKET_DOMAIN="";
-	public final static String PRIVATE_BUCKET="if-pri";
-	public final static String PRIVATE_BUCKET_DOMAIN="http://7qnctm.com1.z0.glb.clouddn.com";
+	public final static String PUBLIC_BUCKET_DOMAIN = "http://7pn64c.com1.z0.glb.clouddn.com";
+	public final static String PRIVATE_BUCKET = "if-pri";
+	public final static String PRIVATE_BUCKET_DOMAIN = "http://7qnctm.com1.z0.glb.clouddn.com";
 }

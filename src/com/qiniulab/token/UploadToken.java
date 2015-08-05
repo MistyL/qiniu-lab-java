1d7cbe4ffe88c74c87a8e170424b04f79802ca8b
Jemy <jemygraw@gmail.com>
2015-08-05 21:51:36 +0800

    update fsizelimit example

--- src/com/qiniulab/token/UploadToken.java
+++ src/com/qiniulab/token/UploadToken.java
@@ -11,24 +11,27 @@ import com.qiniulab.config.LabConfig;
 public class UploadToken {
 	// 生成无key文件的上传凭证
 	public static void createSimpleUploadWithoutKeyToken() {
-		Auth auth = Auth.create(LabConfig.ACCESS_KEY,
-				LabConfig.SECRET_KEY);
+		Auth auth = Auth.create(LabConfig.ACCESS_KEY, LabConfig.SECRET_KEY);
 		String uptoken = auth.uploadToken(LabConfig.PUBLIC_BUCKET);
+		System.out.println(uptoken);
+	}
+
+	public static void createOverwriteUploadToken(String key) {
+		Auth auth = Auth.create(LabConfig.ACCESS_KEY, LabConfig.SECRET_KEY);
+		String uptoken = auth.uploadToken(LabConfig.PUBLIC_BUCKET, key);
 		System.out.println(uptoken);
 	}
 
 	// 生成有key文件的上传凭证(其实和无key文件一样)
 	public static void createSimpleUploadWithKeyToken() {
-		Auth auth = Auth.create(LabConfig.ACCESS_KEY,
-				LabConfig.SECRET_KEY);
+		Auth auth = Auth.create(LabConfig.ACCESS_KEY, LabConfig.SECRET_KEY);
 		String uptoken = auth.uploadToken(LabConfig.PUBLIC_BUCKET);
 		System.out.println(uptoken);
 	}
 
 	// 业务服务器指定上传的文件名称
 	public static void createSimpleUploadWithSaveKeyToken() {
-		Auth auth = Auth.create(LabConfig.ACCESS_KEY,
-				LabConfig.SECRET_KEY);
+		Auth auth = Auth.create(LabConfig.ACCESS_KEY, LabConfig.SECRET_KEY);
 		StringMap policy = new StringMap();
 		policy.put("saveKey", "camera_upload/2015/03/12/file100.mp4");
 		long expires = 3600;// 3600秒＝1小时
@@ -37,10 +40,24 @@ public class UploadToken {
 		System.out.println(uptoken);
 	}
 
+	public static void createCallbackUploadWithSaveKeyToken() {
+		Auth auth = Auth.create(LabConfig.ACCESS_KEY, LabConfig.SECRET_KEY);
+		StringMap policy = new StringMap();
+		policy.put("saveKey", "camera_upload/2015/03/12/file100.mp4");
+		long expires = 3600;// 3600秒＝1小时
+		StringMap putPolicy = new StringMap();
+		putPolicy.put("callbackUrl", "http://d2.qiniu.com/fake/callback");
+		putPolicy
+				.put("callbackBody",
+						"{\"hash\":\"$(hash)\",\"key\":\"$(key)\",\"bucket\":\"$(bucket)\",\"hello\":\"$(x:hello)\"}");
+		String uptoken = auth.uploadToken(LabConfig.PUBLIC_BUCKET, null,
+				expires, policy);
+		System.out.println(uptoken);
+	}
+
 	// 带持久化处理的上传凭证
 	public static void createSimpleUploadWithPfop() {
-		Auth auth = Auth.create(LabConfig.ACCESS_KEY,
-				LabConfig.SECRET_KEY);
+		Auth auth = Auth.create(LabConfig.ACCESS_KEY, LabConfig.SECRET_KEY);
 		StringMap policy = new StringMap();
 		policy.put("persistentOps", "avthumb/mp4;vframe/png/offset/0");
 		policy.put("persistentNotifyUrl", "http://api.abc.com/qiniuFopNotify");
@@ -54,11 +71,10 @@ public class UploadToken {
 
 	// 限制文件的上传大小
 	public static void createSimpleUploadWithFsizeLimit() {
-		Auth auth = Auth.create(LabConfig.ACCESS_KEY,
-				LabConfig.SECRET_KEY);
+		Auth auth = Auth.create(LabConfig.ACCESS_KEY, LabConfig.SECRET_KEY);
 		StringMap policy = new StringMap();
 		int fsizeLimit = 4 * 1024 * 1024;// 4MB，可以限定客户端上传的文件大小不超过4MB
-		policy.put("fsizeLimit", fsizeLimit + "");
+		policy.put("fsizeLimit", new Integer(fsizeLimit));
 
 		long expires = 3600;// 3600秒＝1小时
 		String uptoken = auth.uploadToken(LabConfig.PUBLIC_BUCKET, null,
@@ -68,8 +84,7 @@ public class UploadToken {
 
 	// 限定文件的上传类型
 	public static void createSimpleUploadWithMimeLimit() {
-		Auth auth = Auth.create(LabConfig.ACCESS_KEY,
-				LabConfig.SECRET_KEY);
+		Auth auth = Auth.create(LabConfig.ACCESS_KEY, LabConfig.SECRET_KEY);
 		StringMap policy = new StringMap();
 		policy.put("mimeLimit", "image/png;image/jpeg");
 
@@ -80,7 +95,7 @@ public class UploadToken {
 	}
 
 	public static void main(String[] args) {
-		createSimpleUploadWithPfop();
+		createSimpleUploadWithFsizeLimit();
 	}
 
 }
